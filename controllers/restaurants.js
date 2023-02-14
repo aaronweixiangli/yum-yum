2d719325507cc932fa326cdf5487b93a58bc2165
aaronweixiangli <aaronweixiangli@gmail.com>
2023-02-13 16:37:38 -0800

    updates show.ejs

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,9 +1,33 @@
 const bearer = process.env.YELP_API_KEY;
 const clientId = process.env.YELP_CLIENT_ID;
-const ROOT_URL = 'https://api.yelp.com/v3';
+const ROOT_URL = 'https://api.yelp.com/v3/businesses';
 
 module.exports= {
     index,
+    show
+}
+
+
+async function show(req, res, next) {
+    const id = req.params.id;
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${bearer}`
+        }
+      };
+    try {
+        const restaurant = await fetch(`${ROOT_URL}/${id}`, options)
+            .then(res => res.json());
+        const yelpReviews = await fetch(`${ROOT_URL}/${id}/reviews?limit=20&sort_by=yelp_sort`, options)
+            .then(res => res.json());
+        const error = null;
+        res.render('restaurants/show', {title: 'Restaurant', restaurant, error, yelpReviews});
+    } catch (error) {
+        const restaurant = null;
+        res.render('restaurants/show', {title: 'Restaurant', restaurant, error});
+    }
 }
 
 async function index(req, res, next) {
@@ -17,7 +41,7 @@ async function index(req, res, next) {
         }
       };
     try {
-        const restaurantsData = await fetch(`https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&limit=50`, options)
+        const restaurantsData = await fetch(`${ROOT_URL}/search?location=${location}&term=${term}&limit=50`, options)
             .then(res => res.json())
         const error = null;
         res.render('restaurants/index', {title: 'Restaurants', restaurantsData, error})
